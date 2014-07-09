6ef98adce194545104ac4eb6e72300c2bd8daa66
Maximilian Irro <max@disposia.org>
2014-07-09 19:00:17 +0200

    added the basic classes

--- src/fsinv/viewer/javafx/LookupTable.java
+++ src/fsinv/viewer/javafx/LookupTable.java
@@ -1,46 +1,37 @@
 
 package fsinv.viewer.javafx;
 
-import java.util.Date;
+import java.util.HashMap;
 
 /**
  *
  * @author Maximilian Irro
  */
-public class FileDefinition implements FsEntity{
-	
-	private final String path;
-	private final long bytes;
-	private final Date ctime;
-	private final Date mtime;
-	private final int mimeId;
-	private final int kindId;
-	
-	public FileDefinition(String path, long bytes, Date ctime, Date mtime, int mimeId, int kindId){
-		this.path = path;
-		this.bytes = bytes;
-		this.ctime = ctime;
-		this.mtime = mtime;
-		this.mimeId = mimeId;
-		this.kindId = kindId;
-	}
-
-    @Override
+public class LookupTable {
+    
+    private long idCursor;
+    private HashMap<Long,String> descriptionMap;
+    
+    public LookupTable(){
+    
+    }
+    
+    public String getDescription(long id){
+        return (String) descriptionMap.get(id);
+    }
+    
     public void fromDatabase() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
     public void fromJSON() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
     public void fromXML() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
     public void fromYAML() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
