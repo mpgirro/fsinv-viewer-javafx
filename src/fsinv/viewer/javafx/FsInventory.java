e1a12dc7b6ad6c012da0810b25c5151fc365dfb2
Maximilian Irro <max@disposia.org>
2014-07-10 17:26:30 +0200

    added loading procedure of inventory file; yet the quick-json library can't handle empty JSOn arrays it seems. Great!

--- src/fsinv/viewer/javafx/FsInventory.java
+++ src/fsinv/viewer/javafx/FsInventory.java
@@ -22,12 +22,12 @@ public class FsInventory {
         this.fileStructure = fileStructure;
     }
     
-    public void fromJSON(String inputJsonString){
+    public static FsInventory fromJSON(String jsonInputString){
         
         JsonParserFactory factory = JsonParserFactory.getInstance(); 
         JSONParser parser = factory.newJsonParser();
   
-        Map fsinvData = parser.parseJson(inputJsonString);
+        Map fsinvData = parser.parseJson(jsonInputString);
 
         List kindTabJsonList = (List) fsinvData.get("kind_tab");
         LookupTable kindTab = LookupTable.fromJSON(kindTabJsonList);
@@ -38,6 +38,7 @@ public class FsInventory {
         Map fstructTabMap = (Map) fsinvData.get("file_structure");
         DirectoryDefinition fileStructure = DirectoryDefinition.fromJSON(fstructTabMap);
         
+        return new FsInventory(kindTab, mimeTab, fileStructure);
     }
     
 }
--- src/fsinv/viewer/javafx/FsinvViewerGUIController.java
+++ src/fsinv/viewer/javafx/FsinvViewerGUIController.java
@@ -6,9 +6,15 @@
 
 package fsinv.viewer.javafx;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -42,9 +48,55 @@ public class FsinvViewerGUIController implements Initializable {
         
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open inventory File");
+        
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JSON files (*.json)", "*.json"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("YAML files (*.yal|*.yml)", "*.yaml", "*.yml"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("SQLite3 DB files (*.db|*.sqlite3)", "*.db", "*.sqlite3"));
+        
         File file = fileChooser.showOpenDialog(stage);
         if (file != null){
-            System.out.println("File choosen: " + file.getAbsolutePath());
+            String filePath = file.getAbsolutePath();
+            
+            statusLabel.setText("Loading " + filePath);
+            System.out.println("Loading " + filePath);
+
+            String ext = "";
+            int i = filePath.lastIndexOf('.');
+            int p = Math.max(filePath.lastIndexOf('/'), filePath.lastIndexOf('\\'));
+            if (i > p) 
+                ext = filePath.substring(i+1);
+            
+            FsInventory fsInv = null;
+            switch(ext.toLowerCase()){
+            
+                case "json":
+                    String jsonFileString = "";
+                    try (Scanner scanner =  new Scanner(file.getAbsoluteFile())){
+                      while (scanner.hasNextLine()){
+                        jsonFileString += scanner.nextLine();
+                      }
+                    } catch (FileNotFoundException ex){
+                        Logger.getLogger(FsinvViewerGUIController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    fsInv = FsInventory.fromJSON(jsonFileString);
+                    break;
+                case "yml":
+                case "yaml":
+                    throw new UnsupportedOperationException("Not supported yet."); 
+                    //break;
+                case "db":
+                case "sqlite3":
+                    throw new UnsupportedOperationException("Not supported yet."); 
+                    //break;
+                case "xml":
+                    throw new UnsupportedOperationException("Not supported yet.");    
+                    //break;
+                default:
+                    System.out.println("File extension " + ext + " not valid");
+            } 
+        } else {
+            System.out.println("Choosen file is not a file (file==null)");
         }
         
         //label.setText("Hello World!");
