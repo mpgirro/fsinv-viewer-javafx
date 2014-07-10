4aa9e03cb76b6fdbbae16695cc9baf4677d6b988
Maximilian Irro <max@irro.at>
2014-07-10 22:37:03 +0200

    tree file nodes now get icons based on their mime type (first utilisation of the MIME Table!); not very distinctive yet, and the icons are not the best either, but it works

---
+++ src/fsinv/viewer/javafx/FsInvIconFactory.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fsinv.viewer.javafx;
+
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author Maximilian Irro
+ */
+class FsInvIconFactory {
+    
+    private final LookupTable mimeTab;
+    
+    public FsInvIconFactory(LookupTable mimeTab){
+        this.mimeTab = mimeTab;
+    }
+
+    public Node getIconbyMime(FileStructureEntity fse) {
+        if(fse instanceof DirectoryDefinition)
+            return new ImageView(new Image(getClass().getResourceAsStream("icons/folder_16.png")));
+        else{
+            
+            long mimeId = ((FileDefinition) fse).mimeId;
+            String mimeType = mimeTab.getDescription(mimeId);
+
+            switch( mimeType ){
+                case "text/plain":
+                case "text/x-nfo":
+                    return new ImageView(new Image(getClass().getResourceAsStream("icons/document_16.png")));
+                case "video/x-matroska":
+                case "application/mp4, audio/mp4, video/mp4, video/vnd.objectvideo":
+                case "video/x-msvideo":
+                case "video/x-m4v, video/vnd.objectvideo":
+                    return new ImageView(new Image(getClass().getResourceAsStream("icons/video_16.png")));
+                case "audio/mpeg":
+                    return new ImageView(new Image(getClass().getResourceAsStream("icons/saudio_16.png")));
+                case "image/jpeg":
+                case "image/png":
+                    return new ImageView(new Image(getClass().getResourceAsStream("icons/image_16.png")));
+                default:
+                    return new ImageView(new Image(getClass().getResourceAsStream("icons/file_16.png")));
+            }
+        
+        
+        }
+    }
+
+}
